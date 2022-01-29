8409a838858e6646808a5443def86ed039b86abb
Sebastian Yang <47506432+SCP650@users.noreply.github.com>
2022-01-29 13:35:11 -0500

    auto door works!!!

---
+++ Assets/Scripts/AutoDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    public Animator animator;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            animator.SetBool("character_nearby", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("character_nearby", false);
+        }
+    }
+}

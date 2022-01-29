009f346e183a7b1eb4690847293e2b1221034514
Sebastian Yang <47506432+SCP650@users.noreply.github.com>
2022-01-28 19:59:50 -0500

    equip gun and propel when play

---
+++ Assets/Scripts/TogglePropel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+
+public class TogglePropel : MonoBehaviour
+{
+    public GameObject LeftHandModel;
+    public GameObject RightHandModel;
+    public GameObject PropellerPrefab_L;
+    public GameObject PropellerPrefab_R;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputBridge.Instance.XButton) //left hand
+        {
+            ToggleActive(LeftHandModel);
+            ToggleActive(PropellerPrefab_L);
+        }
+        else if (InputBridge.Instance.AButton) //right hand
+        {
+            ToggleActive(RightHandModel);
+            ToggleActive(PropellerPrefab_R);
+        }
+    }
+    private void ToggleActive(GameObject thing)
+    {
+        thing.SetActive(!thing.activeSelf);
+    }
+     
+}
